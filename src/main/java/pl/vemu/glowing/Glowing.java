97435bd667f66229f32354c8d0a0d1c6642d5469
xVemu <38427679+xVemu@users.noreply.github.com>
2020-09-13 12:04:38 +0200

    init

---
+++ src/main/java/pl/vemu/glowing/Glowing.java
@@ -0,0 +1,10 @@
+package pl.vemu.glowing;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Glowing implements ModInitializer {
+    @java.lang.Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/pl/vemu/glowing/client/GlowingClient.java
@@ -0,0 +1,13 @@
+package pl.vemu.glowing.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class GlowingClient implements ClientModInitializer {
+    @java.lang.Override
+    public void onInitializeClient() {
+
+    }
+}

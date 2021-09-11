4ce26f56fbd708f1ca08f48f2ba251a4430be1c3
xVemu <38427679+xVemu@users.noreply.github.com>
2021-09-11 18:37:24 +0200

    * update dependencies * add loot from block when destroyed by hammer

--- src/main/kotlin/pl/vemu/diamold/Diamold.kt
+++ src/main/kotlin/pl/vemu/diamold/Diamold.kt
@@ -5,15 +5,19 @@ import net.minecraft.block.Block
 import net.minecraft.item.Item
 import net.minecraft.util.Identifier
 import net.minecraft.util.registry.Registry
+import org.apache.commons.io.FileUtils
 import pl.vemu.diamold.blocks.DiamoldBlock
 import pl.vemu.diamold.items.DiamoldItems
 import pl.vemu.diamold.items.JsonName
+import java.io.File
 
 fun init() {
     DiamoldItems.arrayOfItems.forEach(::registerItem)
     registerBlock(DiamoldBlock)
     if (FabricLoader.getInstance().isModLoaded("crushing_hammers")) {
-        registerItem(DiamoldItems.DiamoldHammer) //TODO drop for block and netherite hammer craftable
+        registerItem(DiamoldItems.DiamoldHammer)
+        val inputStream = object {}.javaClass.getResourceAsStream("/configs/diamold_block.json")
+        FileUtils.copyInputStreamToFile(inputStream, File("config/crushing_hammers/recipes/diamold_block.json"))
     }
 }
 

99b19faa0ee7d69b97a72319ba7fe585fef520a2
Kamil Kras <38427679+xVemu@users.noreply.github.com>
2023-08-05 03:15:19 +0200

    Update to 1.20

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,16 +3,16 @@ plugins {
     id("fabric-loom") version "1.3-SNAPSHOT"
 }
 
-version = "1.6.1"
+version = "1.6.2"
 group = "pl.vemu"
 base.archivesName = "diamold"
 
 dependencies {
-    minecraft("com.mojang:minecraft:1.19.4")
-    mappings("net.fabricmc:yarn:1.19.4+build.2:v2")
+    minecraft("com.mojang:minecraft:1.20")
+    mappings("net.fabricmc:yarn:1.20+build.1:v2")
 
     modImplementation("net.fabricmc:fabric-loader:0.14.22")
-    modImplementation("net.fabricmc.fabric-api:fabric-api:0.86.1+1.19.4")
+    modImplementation("net.fabricmc.fabric-api:fabric-api:0.83.0+1.20")
     modImplementation("net.fabricmc:fabric-language-kotlin:1.10.8+kotlin.1.9.0")
 }
 
--- src/main/kotlin/pl/vemu/diamold/Diamold.kt
+++ src/main/kotlin/pl/vemu/diamold/Diamold.kt
@@ -6,6 +6,9 @@ import net.minecraft.block.Block
 import net.minecraft.item.ItemStack
 import net.minecraft.registry.Registries
 import net.minecraft.registry.Registry
+import net.minecraft.registry.RegistryKey
+import net.minecraft.registry.RegistryKeys
+import net.minecraft.text.Text
 import net.minecraft.util.Identifier
 import pl.vemu.diamold.blocks.DiamoldBlock
 import pl.vemu.diamold.items.Items
@@ -14,19 +17,25 @@ import pl.vemu.diamold.items.Items
 fun init() {
     Items.entries.forEach(::registerItem)
     registerBlock(DiamoldBlock)
+    registerGroup()
 }
-
-private val diamoldGroup =
-    FabricItemGroup.builder(Identifier("diamold", "group"))
-        .icon { ItemStack(Items.Ingot.item) }.build()
 
 private fun registerItem(item: Items) {
     Registry.register(Registries.ITEM, Identifier("diamold", item.identifier), item.item)
-    ItemGroupEvents.modifyEntriesEvent(diamoldGroup).register { content ->
+    ItemGroupEvents.modifyEntriesEvent(groupKey).register { content ->
         content.add(ItemStack(item.item))
     }
 }
 
 private fun registerBlock(block: Block) {
     Registry.register(Registries.BLOCK, Identifier("diamold", "diamold_block"), block)
+}
+
+private val groupKey = RegistryKey.of(RegistryKeys.ITEM_GROUP, Identifier("diamold", "diamold_group"))
+
+private fun registerGroup() {
+    val diamoldGroup =
+        FabricItemGroup.builder().displayName(Text.translatable("itemGroup.diamold.group"))
+            .icon { ItemStack(Items.Ingot.item) }.build()
+    Registry.register(Registries.ITEM_GROUP, groupKey, diamoldGroup)
 }
